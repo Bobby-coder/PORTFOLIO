be5bb8cc9f610bdbab096bc8d9a311911e387c24
Bobby-coder <bobbysadhwani612@gmail.com>
2022-12-14 01:05:41 +0530

    blogApi added

---
+++ src/api/blogApi.js
@@ -0,0 +1,22 @@
+const blogApi = [
+    
+    {
+        title: 'JAVASCRIPT OBJECTS',
+        liveLink: 'https://bobby-sadhwani.hashnode.dev/javascript-objects-handbook',
+        dateString: 'September 7, 2022'
+    },
+
+    {
+        title: 'JAVASCRIPT ARRAYS',
+        liveLink: 'https://bobby-sadhwani.hashnode.dev/javascript-array-handbook',
+        dateString: 'September 1, 2022'
+    },
+
+    {
+        title: 'JavaScript - Let, Var & Const',
+        liveLink: 'https://bobby-sadhwani.hashnode.dev/javascript-let-var-const',
+        dateString: 'October 2, 2022'
+    }
+]
+
+export default blogApi;
