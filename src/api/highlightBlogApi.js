d1acc61b96b82c33a9096d72bdf0b30f74415537
Bobby-coder <bobbysadhwani612@gmail.com>
2022-12-14 01:06:13 +0530

    highlighted blog api added

---
+++ src/api/highlightBlogApi.js
@@ -0,0 +1,16 @@
+const highlightBlogApi = [
+    
+    {
+        title: 'JAVASCRIPT OBJECTS',
+        liveLink: 'https://bobby-sadhwani.hashnode.dev/javascript-objects-handbook',
+        dateString: 'September 7, 2022'
+    },
+
+    {
+        title: 'JAVASCRIPT ARRAYS',
+        liveLink: 'https://bobby-sadhwani.hashnode.dev/javascript-array-handbook',
+        dateString: 'September 1, 2022'
+    }
+]
+
+export default highlightBlogApi;
