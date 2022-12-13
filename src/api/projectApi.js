3ee2428cdfa0b20403a1f5277a29caff320117f4
Bobby-coder <bobbysadhwani612@gmail.com>
2022-12-14 01:06:39 +0530

    project blog api added

---
+++ src/api/projectApi.js
@@ -0,0 +1,51 @@
+const projectApi = [
+    {
+        title: 'CRYPTO SPY',
+        liveLink: "https://crypto-spy.netlify.app",
+        githubLink: "https://github.com/Bobby-coder/CRYPTO-SPY",
+        imgSrc: "/images/crypto-spy.webp",
+        description: "Implemented a fully responsive real time Crypto tracker web app with React JS and CoinGecko API. Technologies used : React JS, Chart JS, Tailwind CSS."
+    },
+
+    {
+        title: 'AMAZON CLONE',
+        liveLink: "https://clone-ff7af.web.app",
+        githubLink: "https://github.com/Bobby-coder/Amazon-Clone",
+        imgSrc: `/images/amazon-clone.webp`,
+        description: "Implemented a fully responsive Amazon Clone application. User can do signIn, signUp, add to cart and remove from cart."
+    },
+
+    {
+        title: 'RANDOM QUOTE GENERATOR',
+        liveLink: "https://random-quote-inventor.netlify.app",
+        githubLink: "https://github.com/Bobby-coder/Random-Quote-Generator",
+        imgSrc: `/images/random-quote-generator.webp`,
+        description: "Implemented a Random Quote Generator web app where user can listen, copy and post the quote on twitter."
+    },
+    
+    {
+        title: 'RODE UI CLONE',
+        liveLink: "https://rode-web.netlify.app",
+        githubLink: "https://github.com/Bobby-coder/RODE-UI-CLONE",
+        imgSrc: `/images/rode-clone.webp`,
+        description: "Implemented a fully responsive responsive RODE UI clone. Used Technologies: HTML, TAILWIND CSS."
+    },
+
+    {
+        title: 'PAYTM UI CLONE',
+        liveLink: "https://paytm-web.netlify.app",
+        githubLink: "https://github.com/Bobby-coder/PAYTM-UI-CLONE",
+        imgSrc: `/images/paytm-clone.webp`,
+        description: "Implemented a fully responsive responsive PAYTM UI clone. Used Technologies: HTML, TAILWIND CSS."
+    },
+
+    {
+        title: 'SHOPIFY UI CLONE',
+        liveLink: "https://shopify-web.netlify.app",
+        githubLink: "https://github.com/Bobby-coder/SHOPIFY-UI-CLONE",
+        imgSrc: `/images/shopify-clone.webp`,
+        description: "Implemented a fully responsive responsive SHOPIFY UI clone. Used Technologies: HTML, TAILWIND CSS."
+    }
+]
+
+export default projectApi;
