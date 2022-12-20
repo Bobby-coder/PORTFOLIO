baf2e5ffe14013fbca6cbba5705ec7dff97a4dd9
Bobby-coder <bobbysadhwani612@gmail.com>
2022-12-20 17:53:14 +0530

    description bug fixed

--- src/api/highlightBlogApi.js
+++ src/api/highlightBlogApi.js
@@ -4,14 +4,14 @@ const highlightBlogApi = [
         title: 'JAVASCRIPT OBJECTS',
         liveLink: 'https://bobby-sadhwani.hashnode.dev/javascript-objects-handbook',
         dateString: 'September 7, 2022',
-        description: 'The aim of this article is to simply what Javascript arrays are, how they are defined, displayed, and in most cases when a developer should implement them.'
+        description: 'We all might feel that we know all about objects in JavaScript or at times we feel what the heck is wrong with objects in JS. In this blog we will take learn about objects.'
     },
 
     {
         title: 'JAVASCRIPT ARRAYS',
         liveLink: 'https://bobby-sadhwani.hashnode.dev/javascript-array-handbook',
         dateString: 'September 1, 2022',
-        description: 'We all might feel that we know all about objects in JavaScript or at times we feel what the heck is wrong with objects in JS. In this blog we will take learn about objects.'
+        description: 'The aim of this article is to simply what Javascript arrays are, how they are defined, displayed, and in most cases when a developer should implement them.'
     }
 ]
 
