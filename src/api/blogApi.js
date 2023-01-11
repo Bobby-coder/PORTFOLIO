1050791c3d55e33219b62d7bbbce496b512307de
Bobby-coder <bobbysadhwani612@gmail.com>
2023-01-12 00:05:33 +0530

    new blog added

--- src/api/blogApi.js
+++ src/api/blogApi.js
@@ -1,5 +1,22 @@
 const blogApi = [
   {
+    title: "Prototypal Inheritance in JavaScript",
+    liveLink:
+      "https://bobby-sadhwani.hashnode.dev/prototypal-inheritance-in-javascript",
+    dateString: "January 11, 2023",
+    description:
+      "The aim of this article is to simplify what is prototype and prototypal inheritance in Javascript",
+  },
+
+  {
+    title: "JavaScript - Let, Var & Const",
+    liveLink: "https://bobby-sadhwani.hashnode.dev/javascript-let-var-const",
+    dateString: "October 2, 2022",
+    description:
+      "With the release of ES6, two new ways of variable declaration were introduced in JavaScript. In this blog we will take learn about how let & const work and will see the benefits of using let & const over var.",
+  },
+
+  {
     title: "JAVASCRIPT OBJECTS",
     liveLink: "https://bobby-sadhwani.hashnode.dev/javascript-objects-handbook",
     dateString: "September 7, 2022",
@@ -12,14 +29,7 @@ const blogApi = [
     liveLink: "https://bobby-sadhwani.hashnode.dev/javascript-array-handbook",
     dateString: "September 1, 2022",
     description:
-      "The aim of this article is to simply what Javascript arrays are, how they are defined, displayed, and in most cases when a developer should implement them.",
-  },
-
-  {
-    title: "JavaScript - Let, Var & Const",
-    liveLink: "https://bobby-sadhwani.hashnode.dev/javascript-let-var-const",
-    dateString: "October 2, 2022",
-    description: "With the release of ES6, two new ways of variable declaration were introduced in JavaScript. In this blog we will take learn about how let & const work and will see the benefits of using let & const over var.",
+      "The aim of this article is to simplify what Javascript arrays are, how they are defined, displayed, and in most cases when a developer should implement them.",
   },
 ];
 
